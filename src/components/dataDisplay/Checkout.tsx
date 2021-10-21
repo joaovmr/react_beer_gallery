2b92aadca827ddfb0e4ff11ae7774976109b4143
joaovmr <48189739+joaovmr@users.noreply.github.com>
2021-10-21 14:18:16 -0300

    Checkout Page is displaying the total price and the itens of the cart

--- src/components/dataDisplay/Checkout.tsx
+++ src/components/dataDisplay/Checkout.tsx
@@ -1,6 +1,28 @@
+import { TReducers } from "../store/reducers";
+import { useSelector } from 'react-redux';
+import Card from "./Beer/Card";
 
 const Checkout = () => { 
-    return <h1>PORNO GAY</h1> 
+
+    const beersCatalog = useSelector((state: TReducers) => state.catalogo.beers)
+    const totalStock = useSelector((state: TReducers) => state.total.totals)
+    return (
+        <>
+            {beersCatalog.length !== 0 ? 
+                <div className = 'precoCarrinho'>
+                    <h1>Total Price of the cart: ${totalStock}</h1>
+                </div>
+                 : null }
+            <div className = 'brejasCheckout'>
+            {beersCatalog.length !== 0 ?
+                    beersCatalog.map((beer:any) => {
+                        return (
+                                    <Card key={beer.id} beer = {beer} isInicial = {false} isModal = {true}/>
+                        )
+                    }) : <h1 className = 'noItems'>There are no Items in the Cart</h1>} 
+            </div>    
+        </>
+    )
 }
 
 export default Checkout
